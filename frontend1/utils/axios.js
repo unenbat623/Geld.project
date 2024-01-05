f282fb3616c86d1d36924470242c5c02a14309f7
Unenbat <batbaatarunenbat20@gmail.com>
2024-01-05 17:46:07 +0800

    frontend2 uusgev

--- frontend1/utils/axios.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const BASE_API_URL = process.env.NEXT_PUBLIC_API_URL;
-
-const instanceAxios = axios.create({
-  baseURL: BASE_API_URL,
-});
-
-export default instanceAxios;
--- frontend1/utils/index.js
+++
@@ -1,8 +0,0 @@
-import { FaHome } from "react-icons/fa";
-
-export const getIcons = (iconName = "home", iconColor = "red") => {
-  const icons = {
-    home: <FaHome size={30} color={iconColor} />,
-  };
-  return icons[iconName];
-};
